d0eb8f0c12cf23e2feda6ed2f0c07c8d2ad059f9
”0xAureoleNode“ <aureolenode@gmail.com>
2022-11-30 21:32:55 +0800

    ...

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Counter from './components/Counter';
+import UseStateArray from './components/UseStateArray';
 
 const App = () => {
   return (
     <div>
-      <Counter />
+      <UseStateArray />
     </div>
   );
 };
