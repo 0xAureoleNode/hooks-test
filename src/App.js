ffce72c588fc5bd4b5e5a3d4b794d3e3a1a9fdcd
”0xAureoleNode“ <aureolenode@gmail.com>
2022-11-30 22:32:54 +0800

    setState in Object

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Counter from './components/Counter';
 import UseStateArray from './components/UseStateArray';
+import UseStateObject from './components/UseStateObject';
 
 const App = () => {
   return (
     <div>
-      <UseStateArray />
+      <UseStateObject />
     </div>
   );
 };
