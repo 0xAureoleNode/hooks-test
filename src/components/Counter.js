a801861bc7a1a069afd7e923ccdaeadea062a48e
”0xAureoleNode“ <aureolenode@gmail.com>
2022-11-30 21:28:34 +0800

    set the counter app number color

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,16 +6,27 @@ const Counter = () => {
     setCount(count - 1);
   };
   const addHandler = () => {
-    setCount(count + 1);
+    setCount((count) => count + 1);
+
+    // have previous state
   };
   const resetHandler = () => {
     setCount(0);
   };
+
+  let color = '#444';
+  count >= 1
+    ? (color = 'blue')
+    : count < 0
+    ? (color = 'red')
+    : (color = '#444');
   return (
     <section className="--flex-center --100vh --bg-primary">
       <div className="container --bg-light --p2 --m2">
-        <h3>Regular HTML Page</h3>
-        <h1 className="count">{count}</h1>
+        <h3>React Counter App</h3>
+        <h1 className="count" style={{ color: color }}>
+          {count}
+        </h1>
         <div className="buttons --flex-center">
           <button
             className="subtract --btn --btn-danger"
