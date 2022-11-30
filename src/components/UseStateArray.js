778c0eb8737f92cd47fd8e79163c396030881237
”0xAureoleNode“ <aureolenode@gmail.com>
2022-11-30 21:32:45 +0800

    commit for no reason

---
+++ src/components/UseStateArray.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+
+const UseStateArray = () => {
+  const [people, setPeople] = useState(second);
+  return <div>UseStateArray</div>;
+};
+
+export default UseStateArray;
