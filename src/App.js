b3f5096833db1292d8d3643a172f4a97e42741ba
”0xAureoleNode“ <aureolenode@gmail.com>
2022-11-30 14:22:58 +0800

    init commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
-import React, { useState, createContext } from "react";
-import UserList from "./components/propsDrilling/UserList";
-import { userData } from "./components/propsDrilling/data";
-import UserContext from "./context/userContext";
+import React from "react";
+import GithubUsers from "./components/customHook/GithubUsers";
 
-// 1. Create the context in a seperate file
-// 2. Provide the context to the components
-// 3. Use the context
-
-function App() {
-  const [users, setusers] = useState(userData);
-
-  const deleteUser = (id) => {
-    const newUsers = users.filter((user) => user.id !== id);
-    setusers(newUsers);
-  };
-
+const App = () => {
   return (
-    <UserContext.Provider value={{ users, deleteUser }}>
-      <div className="--flex-center ">
-        <div className="--width-500px --my --p">
-          <h2>Props Drilling</h2>
-          <UserList />
-        </div>
-      </div>
-    </UserContext.Provider>
+    <div>
+      <GithubUsers />
+    </div>
   );
-}
+};
 
 export default App;
