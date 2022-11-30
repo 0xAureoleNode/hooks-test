57c0401c72bbe8576adc036d64da0c923457c403
”0xAureoleNode“ <aureolenode@gmail.com>
2022-11-30 22:13:32 +0800

    added single filter func

--- src/components/UseStateArray.js
+++ src/components/UseStateArray.js
@@ -1,8 +1,40 @@
 import React, { useState } from 'react';
+import { peopleData } from '../people-data';
+import { BiTrash } from 'react-icons/bi';
 
 const UseStateArray = () => {
-  const [people, setPeople] = useState(second);
-  return <div>UseStateArray</div>;
+  const [people, setPeople] = useState(peopleData);
+
+  const removePerson = (id) => {
+    const newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
+  return (
+    <div>
+      <section className="--flex-center --100vh --bg-primary">
+        <div className="container">
+          <h1 className="--color-white">UseState in Array</h1>
+          {people.map((people) => {
+            let { id, name } = people;
+
+            return (
+              <div className="--flex-between --bg-light --my --card" key={id}>
+                <h2>{name}</h2>
+                <BiTrash
+                  color="red"
+                  size={22}
+                  onClick={() => removePerson(id)}
+                />
+              </div>
+            );
+          })}
+          <button className="--btn btn-danger" onClick={() => setPeople([])}>
+            Clear All
+          </button>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default UseStateArray;
